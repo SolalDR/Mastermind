df642e8f2f2cb78ce3b3eab4a032cfc77a728858
SolalDR <solal.dussout-revel@hotmail.fr>
2017-02-07 13:00:48 +0100

    Fixed Warning RulesActivity

--- app/src/main/java/com/solaldussout_revel/mastermind/RulesActivity.java
+++ app/src/main/java/com/solaldussout_revel/mastermind/RulesActivity.java
@@ -1,10 +1,5 @@
 package com.solaldussout_revel.mastermind;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 public class RulesActivity extends MenuParentActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
